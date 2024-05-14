3fa33a7573758a4e7df8fa09e1afa4f1d7e38286
ericsallstrom <eric.sallstrom@gmail.com>
2024-05-14 16:15:15 +0200

    New request models added.

---
+++ WebShopClient/Models/RequestModels/CreateDiscount.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShopClient.Models.RequestModels
+{
+    public class CreateDiscount
+    {
+        [Required(ErrorMessage = "Please select a product for discount.")]
+        [DisplayName("Product")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Percent is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Percent must be a positive value.")]
+        public int Percent { get; set; }
+
+        [Required(ErrorMessage = "Start date is required.")]
+        [DisplayName("Start date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required.")]
+        [DisplayName("End date")]
+        public DateTime EndDate { get; set; }            
+    }
+}
