542672ff85346c3ab6ff408eae3ff45f9b652ad3
ericsallstrom <eric.sallstrom@gmail.com>
2024-05-16 15:28:14 +0200

    Added JsonPropertyName to remaining models.

--- WebShopClient/Models/RequestModels/CreateDiscount.cs
+++ WebShopClient/Models/RequestModels/CreateDiscount.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace WebShopClient.Models.RequestModels
 {
@@ -7,18 +8,22 @@ namespace WebShopClient.Models.RequestModels
     {
         [Required(ErrorMessage = "Please select a product for discount.")]
         [DisplayName("Product")]
+        [JsonPropertyName("product-id")]
         public int ProductId { get; set; }
 
         [Required(ErrorMessage = "Percent is required.")]
         [Range(0, int.MaxValue, ErrorMessage = "Percent must be a positive value.")]
+        [JsonPropertyName("percent")]
         public int Percent { get; set; }
 
         [Required(ErrorMessage = "Start date is required.")]
         [DisplayName("Start date")]
+        [JsonPropertyName("start-date")]
         public DateTime StartDate { get; set; }
 
         [Required(ErrorMessage = "End date is required.")]
         [DisplayName("End date")]
+        [JsonPropertyName("end-date")]
         public DateTime EndDate { get; set; }            
     }
 }
