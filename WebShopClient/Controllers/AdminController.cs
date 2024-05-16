a81fec0aaef4e80170c72ee8c991022b23bc5c2e
Patrik <144246738+PatpetEdu@users.noreply.github.com>
2024-05-17 00:43:55 +0200

    Dahsboard test, exempel

---
+++ WebShopClient/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShopClient.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+         //Möjligt att lägga till fiktiv data tills vi kan skicka data som vi vill visa.
+            return View();
+        }
+    }
+}
