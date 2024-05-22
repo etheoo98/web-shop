fecd8b37c8f6d6899aec4d12546cfaef5a03402e
etheoo98 <etheoo@protonmail.com>
2024-05-22 14:01:10 +0200

    Add more data annotations to Request DTOs

--- WebShopApi/Models/RequestDTOs/CreateCategoryDto.cs
+++ WebShopApi/Models/RequestDTOs/CreateCategoryDto.cs
@@ -5,13 +5,13 @@ namespace WebShop.Models.RequestDTOs;
 
 public class CreateCategoryDto
 {
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
+    [Required(ErrorMessage = "category is required.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "\'name\' must be between {2} to {1} characters.")]
     [JsonPropertyName("name")]
     public string Name { get; set; }
     
-    [Required]
-    [StringLength(100, MinimumLength = 2)]
+    [Required(ErrorMessage = "description is required.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "\'description\' must be between {2} to {1} characters.")]
     [JsonPropertyName("description")]
     public string Description { get; set; }
 }
