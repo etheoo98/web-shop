593853ea286fe9f11c039eb53397534d9734be73
OlofEnar <olof.nordin@gmail.com>
2024-05-27 13:17:35 +0200

    ManageProducts modals

---
+++ WebShopClient/ViewModels/ManageProductsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebShopClient.Models.RequestModels;
+using WebShopClient.Models.ResponseModels;
+
+namespace WebShopClient.ViewModels
+{
+    public class ManageProductsViewModel
+    {
+        public List<Product> Products { get; set; }
+        public EditProduct EditProduct { get; set; }
+        public CreateDiscount CreateDiscount { get; set; }
+    }
+}
